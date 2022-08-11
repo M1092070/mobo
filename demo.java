af014a6b12f498053f4139d25e488999ba51ab0a
M1092070 <110582673+M1092070@users.noreply.github.com>
2022-08-11 18:52:09 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,15 @@
+package java_Ex;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=10,num2=29,sum;
+		sum=num1+num2;
+		System.out.println(+sum);
+	System.out.print("hrllo");
+	
+
+	}
+
+}
